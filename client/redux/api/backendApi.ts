76ca388f3a6574ad7dddbb3375c5e451df3cac30
ereibor <etunere@gmail.com>
2025-07-07 01:39:02 +0100

    deploy to cloud

--- client/redux/api/backendApi.ts
+++ client/redux/api/backendApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const backendApi = createApi({
   reducerPath: 'backendApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:4000/api' }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_BACKEND_API_URL}),
   tagTypes: ['Comment', 'Like', 'Image'],
   
   endpoints: (builder) => ({
